bfe3bee1fef42d9da9edb21f2322e1a2011359d8
Benjamin Sassonko <b@sassonko.de>
2023-01-15 20:25:16 +0100

    Adapted gridsearch parameters to avoid extremely long runtime.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -15,56 +15,44 @@ from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
 
 import string
 
 from sklearn.metrics import classification_report
-from sklearn.multioutput import MultiOutputClassifier, MultiOutputRegressor
+from sklearn.multioutput import MultiOutputClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import hamming_loss
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
-from sklearn.multioutput import MultiOutputClassifier, MultiOutputRegressor
+from sklearn.multioutput import MultiOutputClassifier
 from sklearn.metrics import classification_report
-
 from sklearn.linear_model import SGDClassifier
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 
-
-# load data from database
-database_filepath = sys.argv[1]
-engine = create_engine(f'sqlite:///{database_filepath}')
-
-df = pd.read_sql_table("Messages_Categories", con=engine)
-
-# Optional: Limit number of included rows for testing purposes
-df = df[:500]
-
-X = df["message"]
-Y = df.iloc[:,4:].astype("int")
+import sys
 
 
-# ### 2. Write a tokenization function to process your text data
+def load_data(database_filepath):
+    """ Load data from database and return the messages and categories as separate data frames."""
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table("Messages_Categories", con=engine)
+
+    # Optional: Limit number of included rows for testing purposes
+    # df = df[:500]
+
+    X = df["message"]
+    Y = df.iloc[:,4:].astype("int")
+    category_names = list(Y.columns)
+
+    return X, Y, category_names
+
 
 def tokenize_single(text):
-    """
-    Tokenize text into sentences and then single words. Stopwords are also removed.
-    """
-    
+    """Tokenize text into sentences and then single words. Stopwords are also removed."""
     message = text.lower()
     
     # Remove punctuation
@@ -77,13 +65,13 @@ def tokenize_single(text):
     try:
         words_tokenized = [word_tokenize(sentence) for sentence in sentence_list][0]
     except:
+        # Some strings lead to errors and are replaced with empty strings (e.g. many whitespaces without meaning)
         words_tokenized = [""]
         print(Exception)
         print(f"Text '{text}' replaced with empty string.")
 
     # Remove stopwords
     words_tokenized = [w for w in words_tokenized if w not in stopwords.words("english")]
-    
     
     return words_tokenized
 
@@ -105,42 +93,82 @@ def lemmatize_single(text):
     words_stemmed = [ps.stem(word) for word in words_lemmatized]
     
     return words_stemmed
+    
+
+def build_model():
+    """Build the model pipeline."""
+    pipeline = Pipeline([
+            ('vect', CountVectorizer(tokenizer=lemmatize_single)),
+            ('tfidf', TfidfTransformer()),
+            ('clf', MultiOutputClassifier(SGDClassifier())),
+        ])
+    
+    # Note, including more parameters significantly increases the time needed to train the model, therefore a small selection has been made.
+    gridsearch_parameters = {
+        'clf__estimator__loss': ["hinge", "log_loss", "log", "modified_huber"] #, "squared_hinge", "perceptron", "squared_error", "huber", "epsilon_insensitive", "squared_epsilon_insensitive"],
+        "clf__estimator__alpha" : [0.00005, 0.0001, 0.0005], #[0.00001, 0.00005, 0.0001, 0.0005, 0.001]
+        # "clf__estimator__max_iter" : [500, 1000], #[500, 1000, 2000]
+        # "clf__estimator__learning_rate" : ["optimal", "adaptive"], #["constant", "optimal", "adaptive"]
+        # "clf__estimator__class_weight" : [None, "balanced"]
+    }
+
+    grid_search_pipeline = GridSearchCV(pipeline, param_grid=gridsearch_parameters)
+
+    return grid_search_pipeline
 
 
-# ### 3. Build a machine learning pipeline
+def evaluate_model(model, X_test, Y_test, category_names):
+    """Evaluate the model by predicting the categories and printing the full classification report and hamming loss."""
+    Y_predicted = model.predict(X_test)
 
-categories_colnames = list(Y.columns)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+    # model_classification_report = classification_report(Y_test, Y_predicted, target_names=category_names)
+    # model_hamming_loss = hamming_loss(Y_test, Y_predicted)
+    print(classification_report(Y_test, Y_predicted, target_names=category_names))
+    print(f"Hamming loss: {hamming_loss(Y_test, Y_predicted)}")
 
+    # grid_search_results = pd.DataFrame(model.cv_results_)
+    # grid_search_results
 
-pipeline_modular = Pipeline([
-        ('vect', CountVectorizer(tokenizer=lemmatize_single)),
-#         ('tfidf', TfidfVectorizer(lowercase=False)),
-        ('tfidf', TfidfTransformer()),
-        # ('clf', MultiOutputClassifier(RandomForestClassifier())),
-        # ('clf', MultiOutputClassifier(MultinomialNB(fit_prior=True, class_prior=None))),
-        ('clf', MultiOutputClassifier(SGDClassifier())),
-        # ('clf', MLPClassifier(random_state=1, max_iter=300)),
-    ])
+    # model_classification_report.to_csv("model_classification_report.csv")
 
 
-# ### 4. Train pipeline
-pipeline_modular.fit(X_train, Y_train)
-prediction_modular = pipeline_modular.predict(X_test)
-
-# ### 5. Test your model
-print(classification_report(Y_test, prediction_modular, target_names=categories_colnames))
-print(f"Hamming loss: {hamming_loss(Y_test, prediction_modular)}")
+def save_model(model, model_filepath):
+    """Save the model as a pickle file."""
+    file_model = open(model_filepath, "wb")
+    pickle.dump(model, file_model)
+    file_model.close()
 
 
-# ### 9. Export your model as a pickle file
+def main():
+    """Run the whole pipeline."""
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
 
-# filename_model = "ML_model.sav"
-filename_model = sys.argv[2] #"models/ML_model.pkl"
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print(f"ML pipeline completed. Model exported as picke file '{model_filepath}'")
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
-file_model = open(filename_model, "wb")
-pickle.dump(pipeline_modular, file_model)
-file_model.close()
+if __name__ == '__main__':
+    main()
 
-print(f"ML pipeline completed. Model exported as picke file '{filename_model}'")
+
